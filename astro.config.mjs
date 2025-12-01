5e01406830e0015aca60323c0943f5a87d1d5718
abouthernan <89414181+abouthernan@users.noreply.github.com>
2025-12-01 12:47:46 -0500

    Add astro config for static build

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config'
+   import react from '@astrojs/react'
+   import sitemap from '@astrojs/sitemap'
+
+   export default defineConfig({
+     integrations: [react(), sitemap()],
+     site: 'https://mariaaliciacabrera.com',
+     base: '/',
+     trailingSlash: 'never',
+     output: 'static'
+   })
